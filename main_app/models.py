858a2a71b01b8896d3a195ee1d9528ed36cae4a1
cesarurueta34 <cesarurueta@utexas.edu>
2022-07-07 20:08:16 -0500

    making migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Workout(models.Model):
+
+    workout_name = models.CharField(max_length=100)
+    type = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['created_at']
