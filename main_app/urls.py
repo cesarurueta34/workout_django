e8ecd084ef35cef61b376aceb665556c57e9379a
cesarurueta34 <cesarurueta@utexas.edu>
2022-07-08 20:51:01 -0500

    commit

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
     path('home/', views.Home.as_view(), name="home"),
+    path('workout_list/', views.WorkoutList.as_view(), name="workout_list"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Workout
 
 # Create your views here.
 
@@ -13,4 +14,10 @@ class Home(View):
 class Home(TemplateView):
     template_name = "home.html"
 
+class WorkoutList(TemplateView):
+    template_name = "workout_list.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Workouts"] = Workout.objects.all() # Here we are using the model to query the database for us.
+        return context
