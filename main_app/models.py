153180976252284dd1e5442f42a2b70fb74f5726
cesarurueta34 <cesarurueta@utexas.edu>
2022-07-09 14:57:09 -0500

    updates

--- main_app/models.py
+++ main_app/models.py
@@ -7,6 +7,7 @@ class Workout(models.Model):
     workout_name = models.CharField(max_length=100)
     type = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
+    notes = models.CharField(max_length=500)
     
     def __str__(self):
         return self.workout_name
