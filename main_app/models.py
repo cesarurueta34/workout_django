6179fbb86b2aabc0b7a2f27711dca1d91eeed9f1
cesarurueta34 <cesarurueta@utexas.edu>
2022-07-09 16:52:17 -0500

    updates

--- main_app/models.py
+++ main_app/models.py
@@ -7,10 +7,21 @@ class Workout(models.Model):
     workout_name = models.CharField(max_length=100)
     type = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
-    notes = models.CharField(max_length=500)
-    
+        
     def __str__(self):
         return self.workout_name
 
     class Meta:
         ordering = ['created_at']
+
+
+class Excercise(models.Model):
+
+    name = models.CharField(max_length=150)
+    reps = models.IntegerField(default=0)
+    sets = models.IntegerField(default=0)
+    weight = models.IntegerField(default=0)
+    Workout = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name="excercises")
+
+    def __str__(self):
+        return self.name
