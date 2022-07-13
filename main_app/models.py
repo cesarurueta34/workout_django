55f42e3265251bb8d57d4915dba794b71ed4c25a
cesarurueta34 <cesarurueta@utexas.edu>
2022-07-12 21:41:49 -0500

    login stuff

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,7 +8,8 @@ class Workout(models.Model):
     workout_name = models.CharField(max_length=100)
     type = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
-        
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)    
+    
     def __str__(self):
         return self.workout_name
 
