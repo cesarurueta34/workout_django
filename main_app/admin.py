d1b5aa33e08b8176fcb1afaa705a2db07f54158d
cesarurueta34 <cesarurueta@utexas.edu>
2022-07-23 14:42:44 -0500

    update

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Workout, Exercise # import the Artist model from models.py
+from .models import Workout, Exercise 
 # Register your models here.
 
 admin.site.register(Workout) # this line will add the model to the admin panel
